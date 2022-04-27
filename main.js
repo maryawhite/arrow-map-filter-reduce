912530fe2e600cb003a0d87ae3b6deaf34b2d9d2
Mary White <mary.a.white28@gmail.com>
2022-04-27 14:47:55 -0400

    added in a couple of arrow functions and a recursive function

--- main.js
+++ main.js
@@ -25,7 +25,6 @@ console.log(addNumsArrow(3, 5));
 
 //not an arrow function, just an interesting example
 //write a function that will reverse a word if it's equal to or more than 5 characters, otherwise return the word normally
-console.log("kata?");
 function spinWords(sentence){
     function reverse(s) {
         return s.split('').reverse().join('');
@@ -50,6 +49,67 @@ console.log(spinWords("Mary bobsleds when it snows"));
 
 
 
+//write an arrow function to sum 2 numbers
+let sum = (a, b) => {
+    return a + b;
+}
+
+let sumB = (a, b) => a + b;
+
+console.log(sum(7,3));    // 10
+console.log(sumB(8, 4));  // 12
+
+
+//write an arrow function to subtract 2 numbers
+let subtract = (x, y) => {
+    return x - y;
+}
+
+let subtractB = (x, y) => x - y;
+
+console.log(subtract(10,7));   // 3
+console.log(subtractB(12,8));  // 4
+
+//write a function that takes in 2 numbers and returns their minimum (the smallest number), using Math.min
+function min(a, b) {
+    return Math.min(a, b);
+}
+
+//rewrite it as an arrow function
+let min2 = (a, b) => Math.min(a,b);
+
+console.log(min(-10, 10));  // -10
+console.log(min(0, 10));    // 0
+console.log(min(10, 150));   // 10
+
+console.log(min2(-10, 10));  // -10
+console.log(min2(0, 10));    // 0
+console.log(min2(10, 150));   // 10
+
+
+//zero is even, 1 is odd, for any other number N, its evenness is the same as N -2. Write a recursive function corresponding to this description
+//return a boolean
+function isEven(num) {
+    if(num === 0) {
+        return true;
+    } else if(num === 1) {
+        return false;
+    } else if(num < 0) {
+        return isEven(-num);  //to account for negative numbers, otherwise you'll get a callStack size error if you try to pass -1
+    } else {
+       return (isEven(num - 2));
+    }
+}
 
 
 
+
+console.log(isEven(0));  //true
+console.log(isEven(1));  //false
+console.log(isEven(2));  //true
+console.log(isEven(8));  //true
+console.log(isEven(9));  //false
+console.log(isEven(50));  //true
+console.log(isEven(75));  //false
+console.log(isEven(-1));  //false
+console.log(isEven(-2));  //true
